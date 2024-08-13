ba4a76c5296ef0fcec4fa59169d6e04f1149324e
Yuda Kurnia Nurul Fikri <yudakurnia.nurulfikri@gmail.com>
2024-08-13 08:59:10 +0700

    need more learning hour for this drawer, change to grid

--- src/Interfaces/App.tsx
+++ src/Interfaces/App.tsx
@@ -1,23 +1,144 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@fontsource-variable/plus-jakarta-sans'
+import VideoJS from './components/VideoJS'
 
-import Button from '@mui/material/Button';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007C6C',
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+  typography: {
+    fontFamily: [
+      '"Plus Jakarta Sans"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(',')
+  },
+});
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
+  const drawerWidth = width*(3/12);
+
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    });
+  };
 
   return (
     <>
-      <Header title="My header" subtitle="subtitle2" />
-      <Main message="My content" />
-      <Button variant="contained">Hello world</Button>
-      <Footer note="Footer Note" />
+      <ThemeProvider theme={theme}>
+        <Box sx={{
+          display: 'flex'
+        }}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+          >
+            <Toolbar>
+              <Typography variant="h5" noWrap component="div">
+                SD Islam Pembangunan
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Drawer
+            sx={{
+              width: drawerWidth/2,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+              },
+            }}
+            variant="permanent"
+            anchor="left"
+          >
+
+            <Divider />
+            <List>
+              <ListItem><img src={'SD Islam Pembangunan.png'}/></ListItem>
+              <Divider />
+              <ListItem>
+
+                <Typography variant="h3" component="div">
+                  Today's Statistics
+                </Typography>
+              </ListItem>
+
+
+            </List>
+          </Drawer>
+          <Box
+            gap={1}
+            component="main"
+            sx={{
+              bgcolor: 'background.default',
+              minWidth: 1200,
+            }}
+          >
+
+          <>
+            <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+          </>
+
+          </Box>
+        </Box>
+
+      </ThemeProvider>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Interfaces/App.tsx'
 import './Interfaces/index.css'
+// Supports weights 200-800
+import '@fontsource-variable/plus-jakarta-sans';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
